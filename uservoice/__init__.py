34a3b5c626e077907c46835b1759a818b3fc332a
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-08-14 15:50:54 -0700

    Make 2-legged calls with the help of tweepy, Twitter API lib.

--- uservoice/__init__.py
+++ uservoice/__init__.py
@@ -5,11 +5,11 @@ import urllib
 import operator
 import array
 import simplejson as json
-import urllib  
+import urllib
 import urllib2
 import datetime
 import pytz
-from tweepy import oauth  
+from tweepy import oauth
 
 def generate_sso_token(subdomain_name, sso_key, user_attributes):
     current_time = (datetime.datetime.now(pytz.utc) + datetime.timedelta(minutes=5)).strftime('%Y-%m-%d %H:%M:%S')
@@ -21,10 +21,9 @@ def generate_sso_token(subdomain_name, sso_key, user_attributes):
     salted = sso_key + subdomain_name
     saltedHash = hashlib.sha1(salted).digest()[:16]
 
-    json_bytes = array.array('b', user_json[0 : len(user_json)]) 
+    json_bytes = array.array('b', user_json[0 : len(user_json)])
     iv_bytes = array.array('b', iv[0 : len(iv)])
 
-    # # xor the iv into the first 16 bytes.
     for i in range(0, 16):
         json_bytes[i] = operator.xor(json_bytes[i], iv_bytes[i])
 
@@ -34,3 +33,20 @@ def generate_sso_token(subdomain_name, sso_key, user_attributes):
     encrypted_bytes = aes.encrypt(data)
 
     return urllib.quote(base64.b64encode(encrypted_bytes))
+
+class OAuth:
+    def __init__(self, subdomain_name, api_key, api_secret):
+        self.api_url = "https://" + subdomain_name + ".uservoice.com"
+        self.consumer = oauth.OAuthConsumer(api_key, api_secret)
+
+    def request(self, method, path, params={}):
+        url = self.api_url + path
+        request = oauth.OAuthRequest.from_consumer_and_token(
+           self.consumer, http_method=method.upper(), http_url=url, parameters={})
+        request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, None)
+
+        headers = request.to_header()
+
+        req = urllib2.Request(url, None, headers)
+
+        return json.load(urllib2.urlopen(req))
