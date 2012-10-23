d41e7a33b8535df276ac7be9d1883f6591d97d38
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-10-23 13:53:25 -0700

    Add long description.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ except ImportError:
 setup(name='uservoice',
       version='v0.0.1',
       description='UserVoice Python library',
+      long_description=open('README.md').read(),
       author='Raimo Tuisku',
       author_email='dev@uservoice.com',
       url='http://developer.uservoice.com/',
