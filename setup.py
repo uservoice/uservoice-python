96a1a8601670622fed362fdb2858b844b3a61db1
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-10-23 14:03:55 -0700

    Correct README path

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 setup(name='uservoice',
       version='v0.0.1',
       description='UserVoice Python library',
-      long_description=open('README.md').read(),
+      long_description=open('README').read(),
       author='Raimo Tuisku',
       author_email='dev@uservoice.com',
       url='http://developer.uservoice.com/',
