268eb5c4b657bf6423b821c7efc82c810a9f514c
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-10-23 14:11:26 -0700

    Use README.md, not README.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 setup(name='uservoice',
       version='v0.0.1',
       description='UserVoice Python library',
-      long_description=open('README').read(),
+      long_description=open('README.md').read(),
       author='Raimo Tuisku',
       author_email='dev@uservoice.com',
       packages=['uservoice'],
