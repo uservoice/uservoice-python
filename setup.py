ab73d66c1e82ee8343d501b7212814189a1ee950
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-01-15 12:43:10 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='uservoice',
-      version='0.0.16',
+      version='0.0.17',
       description='UserVoice Python library',
       url = 'http://pypi.python.org/pypi/uservoice/',
       long_description=re.sub(r'```[^\s]*', '', open('README.md').read()),
